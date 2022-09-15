8567e7587e50238f42d08a2444c1d6a37684e568
Abbas_Mitter <abbasmitter@socialtop.app>
2022-09-15 12:43:30 +0300

    New

---
+++ app/config.php
@@ -0,0 +1,8 @@
+<?php
+//Config Database
+define('DB_HOST', '109.236.84.61');
+define('DB_USER', 'panel_db');
+define('DB_PASS', 'xnUcqff2MG1PljK3');
+define('DB_NAME', 'panel_db');
+define('TIMEZONE', 'Africa/Abidjan');
+define('ENCRYPTION_KEY', '39006e6f50e1b750dfc0133472618714');
